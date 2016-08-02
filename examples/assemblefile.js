0087a212e1688db79877f3af806fe59bc97ac979
Brian Woodward <brian.woodward@gmail.com>
2016-08-02 18:21:39 -0400

    add advanced task in example assemblefile

--- examples/assemblefile.js
+++ examples/assemblefile.js
@@ -14,42 +14,51 @@ var extname = require('gulp-extname');
 var path = require('path');
 
 var collection = require('..');
+var dest = path.join.bind(path, __dirname, 'dist');
 
 var app = assemble();
 app.use(collection());
 
-var dest = path.join.bind(path, __dirname, 'dist');
-app.create('lists');
+app.task('default', ['simple']);
 
 app.task('load-simple', function(cb) {
-  app.lists('*.hbs', {cwd: path.join(__dirname, 'src/templates/lists')})
   app.partials('*.hbs', {cwd: path.join(__dirname, 'src/templates/partials')})
   app.pages('*.hbs', {cwd: path.join(__dirname, 'src')});
   cb();
 });
 
 app.task('simple', ['load-simple'], function() {
-  return app.createIndex('pages', {
-      File: app.View,
+  return app.toStream('pages')
+    .pipe(app.createIndex('tags'))
+    .pipe(extname())
+    .pipe(app.renderFile())
+    .pipe(app.dest(dest('simple')));
+});
+
+app.task('load-advanced', function(cb) {
+  app.create('lists');
+  app.lists('*.hbs', {cwd: path.join(__dirname, 'src/templates/lists')})
+  app.partials('*.hbs', {cwd: path.join(__dirname, 'src/templates/partials')})
+  app.pages('*.hbs', {cwd: path.join(__dirname, 'src')});
+  cb();
+});
+
+app.task('advanced', ['load-advanced'], function() {
+  return app.toStream('pages')
+    .pipe(app.createIndex({
       pattern: ':tags/:tag/page/:pager.idx/index.hbs',
       list: app.lists.getView('list'),
       item: app.lists.getView('item'),
-      paginate: 3,
-      groupFn: function(group) {
-        app.data({site: {tags: group}})
-      }
-    })
+      paginate: 3
+    }))
     // rename `.hbs` to `.html`
     .pipe(extname())
     // buffer files before rendering to ensure context has been created
     .pipe(buffer())
     .pipe(app.renderFile())
     // write rendered files to the destination directory
-    .pipe(app.dest(dest('simple')));
-
+    .pipe(app.dest(dest('advanced')));
 });
-
-app.task('default', ['simple']);
 
 function buffer() {
   var files = [];
