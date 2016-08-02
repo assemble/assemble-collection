15eaeb5993c9a2975435a35764f62c7174edd032
Brian Woodward <brian.woodward@gmail.com>
2016-08-02 18:59:22 -0400

    update example to have a `simple` and `advanced` task `simple` task just passes the property `tags` to group on and uses all the defaults `advanced` task passes the permalink structure, list and item page templates, and pagination settings

--- examples/assemblefile.js
+++ examples/assemblefile.js
@@ -21,19 +21,33 @@ app.use(collection());
 
 app.task('default', ['simple']);
 
+/**
+ * Only load partials and pages for the simple example.
+ */
+
 app.task('load-simple', function(cb) {
   app.partials('*.hbs', {cwd: path.join(__dirname, 'src/templates/partials')})
   app.pages('*.hbs', {cwd: path.join(__dirname, 'src')});
   cb();
 });
 
+/**
+ * The simple example uses defaults to create index pages for the `tags` property found on pages' front-matter
+ */
+
 app.task('simple', ['load-simple'], function() {
   return app.toStream('pages')
     .pipe(app.createIndex('tags'))
     .pipe(extname())
+    .pipe(buffer())
     .pipe(app.renderFile())
     .pipe(app.dest(dest('simple')));
 });
+
+/**
+ * Load templates for the advanced example.
+ * This creates a new views collection called `lists` that contains templates for the `list` and `item` index pages.
+ */
 
 app.task('load-advanced', function(cb) {
   app.create('lists');
@@ -43,13 +57,20 @@ app.task('load-advanced', function(cb) {
   cb();
 });
 
+/**
+ * The advanced example uses a permalink pattern to determine the property to create index pages with and what the `file.path`
+ * structure will look like when the `item` index pages are created.
+ * This example also passes in custom `list` and `item` index page templates from the `lists` views collection.
+ * Pagination is used for the `item` index pages by setting a `limit` on the `paginate` property
+ */
+
 app.task('advanced', ['load-advanced'], function() {
   return app.toStream('pages')
     .pipe(app.createIndex({
       pattern: ':tags/:tag/page/:pager.idx/index.hbs',
       list: app.lists.getView('list'),
       item: app.lists.getView('item'),
-      paginate: 3
+      paginate: {limit: 3}
     }))
     // rename `.hbs` to `.html`
     .pipe(extname())
@@ -68,7 +89,6 @@ function buffer() {
   }, function(cb) {
     var stream = this;
     files.forEach(function(file) {
-      // console.log(file);
       stream.push(file);
     });
     cb();
